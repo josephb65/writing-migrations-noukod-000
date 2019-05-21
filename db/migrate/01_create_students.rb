90f4d3c944ef8c25cdd316cce7f4bae69c032303
Billy Joseph <ingbillyjoseph@gmail.com>
2019-05-21 15:15:46 +0000

    Automatically backed up by Learn

--- db/migrate/01_create_students.rb
+++ db/migrate/01_create_students.rb
@@ -1,7 +1,7 @@
 class CreateStudents < ActiveRecord::Migration[5.1]
   def change
-    create_table do :students |t|
-         t.string :name 
-       end 
-     end 
+    create_table :students do |t|
+      t.string :name
+    end
+  end
 end
